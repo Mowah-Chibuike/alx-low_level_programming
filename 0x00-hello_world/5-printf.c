cec9d21aeb69ee66543fe5ff0697b46f32019cdb
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-08 18:45:19 -0700

    added the 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints a string
+ *
+ * Description - prints with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: prints 0 if successful
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", str);
+	return (0);
+}
