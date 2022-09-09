f6cd7970f1edff213ff87d5be0dc19fc0b982c05
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-08 18:02:59 -0700

    added the 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints out a string
+ *
+ * Description:  prints exactly 'Programming is like building a multilingual puzzle', followed by a new line.
+ *
+ * Returns: function returns 0
+ */
+
+char main(void)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return 0;
+}
