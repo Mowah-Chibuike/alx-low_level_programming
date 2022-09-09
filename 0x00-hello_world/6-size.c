bf829961edb356652fac925ce34b62a00b0eb1e1
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-08 19:32:16 -0700

    added the 6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,10 +15,10 @@ int main(void)
 	long long int l;
 	float m;
 
-	printf("Size of a char: %u byte(s).\n", sizeof(i));
-	printf("Size of an int: %u byte(s).\n", sizeof(j));
-	printf("Size of a long int: %u byte(s).\n", sizeof(k));
-	printf("Size of a long long int: %u byte(s).\n", sizeof(l));
-	printf("Size of a float: %u byte(s).\n", sizeof(m));
+	printf("Size of a char: %u byte(s)\n", sizeof(i));
+	printf("Size of an int: %u byte(s)\n", sizeof(j));
+	printf("Size of a long int: %u byte(s)\n", sizeof(k));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(l));
+	printf("Size of a float: %u byte(s)\n", sizeof(m));
 	return (0);
 }
