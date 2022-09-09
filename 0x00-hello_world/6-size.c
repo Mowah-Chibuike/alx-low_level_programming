b1e9d6346ac338caf02cab0ebf6a0c7834885540
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-08 19:29:13 -0700

    added the 6-size.c file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints the sizes of data  types
+ *
+ * Description - prints the storage values of some data types
+ *
+ * Return: prints 0 if successful
+ */
+
+int main(void)
+{
+	char i;
+	int j;
+	long int k;
+	long long int l;
+	float m;
+
+	printf("Size of a char: %u byte(s).\n", sizeof(i));
+	printf("Size of an int: %u byte(s).\n", sizeof(j));
+	printf("Size of a long int: %u byte(s).\n", sizeof(k));
+	printf("Size of a long long int: %u byte(s).\n", sizeof(l));
+	printf("Size of a float: %u byte(s).\n", sizeof(m));
+	return (0);
+}
