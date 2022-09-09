a6d7152a0a642ce67350c24b96adbee248b55721
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-08 20:49:10 -0700

    added the 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -prints an error message
+ *
+ * Description: main prints out an error mmeesage to the stderr
+ *
+ * Return : returns 1 because it fails
+ */
+
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
