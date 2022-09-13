2220d719328467950cb3a5fe88cf828b8abc6781
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-12 18:40:21 -0700

    added the 102-print_comb5.c file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,8 +8,43 @@
  */
 int main(void)
 {
-	int i = 9;
+	int i;
+	int j;
+	int k;
+	int l;
 
-	putchar(i + '0');
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (l = j + 1; l <= 9; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if ((i == 9)
+					&& (j == 8)
+					&& (k == 9)
+					&& (l == 9))
+					{
+						putchar('\n');
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+			if ((i == 9) && (j == 8))
+			{
+				j += 1;
+			}
+		}
+	}
 	return (0);
 }
