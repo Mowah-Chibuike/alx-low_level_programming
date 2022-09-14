98b7dc8829e746c1d8dc66a8bcc96a2a51556bfd
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-14 10:07:42 -0700

    added the 104-fibonacci.c file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 98 numbers in a fibonacci
+ * sequence that starts with 1 & 2
+ *
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	long long int i = 1, j = 2;
+	int count = 2;
+
+	printf("1, 2, ");
+	while (count <= 96)
+	{
+		i = i + j;
+		j = j + i;
+		printf("%llu, %llu", i, j);
+		if (count != 96)
+		{
+			printf(", ");
+		}
+		count += 2;
+	}
+	printf("\n");
+	return (0);
+}
