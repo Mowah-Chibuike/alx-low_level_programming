f77c4bbd21eaff35afe4d0d15b30d1e9dac1fc54
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-13 19:32:42 -0700

    added the 100-times_table.c file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ void print_times_table(int n)
 						_putchar(' ');
 					_putchar(res + '0');
 				}
-				if (iterations < 9)
+				if (iterations < n)
 				{
 					_putchar(',');
 					_putchar(' ');
