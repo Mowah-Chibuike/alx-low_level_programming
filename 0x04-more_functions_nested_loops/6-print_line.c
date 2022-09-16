27062344d742f939d6bf06889495046ee4090736
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-15 19:40:51 -0700

    added the 6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints _ n times
+ * @n: number of times _ is to be printed
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		i--;
+	}
+	_putchar('\n');
+}
