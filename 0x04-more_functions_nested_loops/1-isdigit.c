f5629d80cd10965c6d24ff3a2619ed0bb3240b61
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-15 18:34:36 -0700

    added the 1-isdigit.c file

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if a character is a singlee digit
+ * @c: chacter to be checked
+ *
+ *
+ * Return: 1 if character is a digit. 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	if ((c >= '0') && (c <= '9'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
