88d5d7e8bc8db3735dcf10d106b5c036d3711e00
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-15 21:26:29 -0700

    added the 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,13 +17,13 @@ int main(void)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
 		{
-			printf("Fizz Buzz");
+			printf("FizzBuzz");
 		}
 		else if ((i % 3) == 0)
 		{
 			printf("Fizz");
 		}
-		else if ((i % 5) == 0 )
+		else if ((i % 5) == 0)
 		{
 			printf("Buzz");
 		}
