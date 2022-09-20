7618c2d0c9c69c3b2e1500e814358b0459634311
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-20 03:00:29 -0700

    added the 5-rev_string.c file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,28 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_length - calculates teh length of a string
+ * @s: the string to be checked
+ *
+ *
+ * Return: the length of s
+ */
+
+int get_length(char *s)
+{
+	int len = 0;
+
+	while (len >= 0)
+	{
+		if (*(s + len) == '\0')
+		{
+			break;
+		}
+		len++;
+	}
+	return (len);
+}
 
 /**
  * rev_string - prints the reverse of a string
@@ -11,18 +34,19 @@
 
 void rev_string(char *s)
 {
-	int p = sizeof(s);
+	int len = get_length(s);
+	char *p = s;
+	char *str = malloc(sizeof(char) * len);
+	int i, b = 0;
 
-	printf("%d\n", p);
-	/*while (i <= len)
+	for (i = 0; s[i] != '\0'; i++)
+		str[i] = s[i];
+	str[i] = '\0';
+
+	while (i >= 0)
 	{
-		*(p + i) = *(s + i);
-		i++;
+		*(p + (i - 1)) = str[b];
+		i--;
+		b++;
 	}
-	while (len > 0)
-	{
-		*(s + (sizeof(s) - len)) = p[len];
-		len--;
-	}
-	_putchar('\n');*/
 }
