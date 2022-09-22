b4ba9bd5ac06ae3201b14fed160aaa40d955a07d
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-22 13:17:50 -0700

    added the headerfile

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void rev_string(char *s);
+void rev_string2(char *s);
+char *_strcat(char *desc, char *src);
+char *_strncat(char *desc, char *src, int n);
+char *_strncpy(char *desc, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *s);
+char *cap_string(char *s);
+char *leet(char *s);
+
+#endif /* MAIN_H */
