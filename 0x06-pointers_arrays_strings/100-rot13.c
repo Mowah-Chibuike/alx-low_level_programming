3b0be9335d3dc3ad1869398ebe110601a9b37f24
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-23 05:04:46 -0700

    added the 100-rot13.c file

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,30 +8,18 @@
  */
 char *rot13(char *s)
 {
-	int i, j, m, n;
-	char alpha1[] = "AaBbCcDdEeFfGgHhIiJjKkLlMm";
-	char alpha2[] = "NnOoPpQqRrSsTtUuVvWwXxYyZz";
+	int i, m;
+	char alpha1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alpha2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (((s[i] >= 'A') && (s[i] <= 'Z')) || ((s[i] >= 'a') && (s[i] <= 'z')))
+		for (m = 0; alpha1[m] != '\0'; m++)
 		{
-			for (m = 0; alpha1[m] != '\0'; m++)
+			if (s[i] == alpha1[m])
 			{
-				if (s[i] == alpha1[m])
-				{
-					s[i] = alpha2[m];
-					break;
-				}
-			}
-
-			for (n = 0; alpha2[n] != '\0'; n++)
-			{
-				if (s[i] == alpha2[n])
-				{
-					s[i] = alpha1[n];
-					break;
-				}
+				s[i] = alpha2[m];
+				break;
 			}
 		}
 	}
