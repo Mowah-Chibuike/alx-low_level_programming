c9639e15f8f7a79d0fd64d5f876d785dc2e060d3
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-26 15:57:54 -0700

    added the 8-print_diagsums.c file

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,17 +10,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, d1 = 0, d2 = 0;
+	int i, j, p, y = 0, z = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		p = (i * size) + i;
-		d1 += *(a + p);
+		y += *(a + p);
 	}
 	for (j = 0; j < size; j++)
 	{
 		p = (j * size) + (size - 1 - j);
-		d2 += *(a + p);
+		z += *(a + p);
 	}
 	printf("%d, %d\n", d1, d2);
 }
