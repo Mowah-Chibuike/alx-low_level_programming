efcd8a192092891d92db87ce2c581b506854ed97
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-28 14:08:00 -0700

    added the 100-is_palindrome.c file

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _strlen - function returns the length of a string
+ * @s: string to be checked
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen(s + 1));
+}
+
+/**
+ * palindome_check - function checks if a string is a palindome
+ * @i: first index
+ * @j: second index
+ * @p: string to be checked
+ *
+ * Return: 1 if a string is a palindrome and 0 if not.
+ */
+
+int palindome_check(int i, int j, char *p)
+{
+	if (i >= j)
+	{
+		return (1);
+	}
+	if (p[j] != p[i])
+	{
+		return (0);
+	}
+
+	return (palindome_check(i + 1, j - 1, p);
+}
+
+/**
+ * is_palindrome - calls palindome_check
+ * @s: string to be checked
+ *
+ * Return: 1 if a string is a palindome and 0 if not.
+ */
+
+int is_palindrome(char *s)
+{
+	int l;
+
+	l = _strlen(s) - 1;
+	return (palindome_check(0, l, s);
+}
