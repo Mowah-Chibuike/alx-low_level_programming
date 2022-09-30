272939fe1364266749bedc9babcaeac6b4893b83
Mowah Chibuike <mctimmykings@gmail.com>
2022-09-30 13:47:34 -0700

    added the 2-args.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives.
+ * @argc: number of arguments passed to the function
+ * @argv: arguments passed into the function
+ *
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]); 
+	}
+
+	return (0);
+}
