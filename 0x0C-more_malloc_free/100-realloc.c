1940a5e4f5643046abf57c3816d7481334e50446
Mowah Chibuike <mctimmykings@gmail.com>
2022-10-06 18:02:40 -0700

    fixed documentation issues

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _realloc: reallocates a memory block using malloc and free
+ * _realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previously allocated with a call to malloc
  * @old_size: the size, in bytes, of the allocated space for ptr
  * @new_size: new size, in bytes of the new memory block
