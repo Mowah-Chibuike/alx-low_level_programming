fdd8f25432eb8d1f7e863003fc534ccea3950ab4
Mowah Chibuike <mctimmykings@gmail.com>
2022-10-14 06:33:52 -0700

    updated the variadic_functions header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,5 +5,17 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+/**
+ * struct fmt - Struct fmt
+ *
+ * @op: The printing format
+ * @f: The function associated
+ */
+typedef struct fmt
+{
+	char *fm;
+	void (*f)(va_list valist);
+} fmt_t;
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
