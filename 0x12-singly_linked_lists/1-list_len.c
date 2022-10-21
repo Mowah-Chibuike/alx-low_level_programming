0feb0772ececfb72dde353a5f735501ccbfce06a
Mowah Chibuike <mctimmykings@gmail.com>
2022-10-21 02:10:53 +0000

    added the 1-list_len.c file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a list
+ * @h: pointer to first node of the list
+ *
+ * Return: number of elements in a list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_h *temp = h;
+	size_t = n;
+
+	while (temp != NULL)
+	{
+		n++;
+		temp = temp->next;
+	}
+	return (n);
+}
