46fef4b9ca9f0e2598aede04ad679b81b8686410
Mowah Chibuike <mctimmykings@gmail.com>
2022-11-01 00:11:49 +0000

    handled edge cases properly

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int len = strlen(text_content);
+	int len;
 	ssize_t n;
 
 	if (filename == NULL)
@@ -23,10 +23,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		n = write(fd, "", 0);
-	else
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
 		n = write(fd, text_content, len);
+	}
 	if (n == -1 || n < (ssize_t)len)
 		return (-1);
 	close(fd);
