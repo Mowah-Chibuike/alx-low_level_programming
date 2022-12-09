25c5793a4b80a959579ed579b93bdee81853dbc3
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-09 02:39:36 +0000

    checked all possible error cases

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t
@@ -29,6 +30,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		if (temp == NULL && i <= index)
 			return (-1);
 		temp = temp->next;
+	}
+	if (temp == NULL)
+	{
+		printf("Hello %d\n", i);
+		return (-1);
 	}
 	prev = temp->prev;
 	next = temp->next;
