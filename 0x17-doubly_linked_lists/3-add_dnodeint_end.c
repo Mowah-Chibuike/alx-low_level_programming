20f22608f8052c8ce60686440e258b10067d2b6b
Mowah Chibuike <mctimmykings@gmail.com>
2022-12-09 00:28:21 +0000

    added the 3-add_dnodeint_end.c file

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * create_new_node - creates a new node in a dlistint_t list
+ * @n: integer to be added in new node
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *create_new_node(int n)
+{
+	dlistint_t *newNode;
+
+	newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+	return (newNode);
+}
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
+ * @head: double pointer to the head node
+ * @n: integer to be inserted in the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *temp = *head, *newNode;
+
+	newNode = create_new_node(n);
+	if (newNode == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = newNode;
+	newNode->prev = temp;
+	return (newNode);
+}
--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -23,3 +23,4 @@ Tasks
 	- 0-print_dlistint.c: contains a function that prints all the elements of a dlistint_t list.
 	- 1-dlistint_len.c: contains a function that returns the number of elements in a linked dlistint_t list.
 	- 2-add_dnodeint.c: contains a function that adds a new node at the beginning of a dlistint_t list.
+	- 3-add_dnodeint_end.c: contains a function that adds a new node at the end of a dlistint_t list.
