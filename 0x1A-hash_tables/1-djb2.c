5fd97ddd9350436553dc6cb0c3c45a250aff723d
Mowah Chibuike <mctimmykings@gmail.com>
2023-01-06 16:27:35 +0000

    handled betty errors

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -3,7 +3,7 @@
 /**
  * hash_djb2 - a hash function implementing the djb2 algorithm.
  * @str: key from which index would be generated
- * 
+ *
  * Return: index associated with the key identified by str
  */
 unsigned long int hash_djb2(const unsigned char *str)
